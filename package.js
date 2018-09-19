4f5a5c49272ebc0d592b6bf197c50d21c47a9ad7
Theodor Diaconu <diaconu.theodor@gmail.com>
2018-09-19 11:18:20 +0300

    Bump to 3.3.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 
 Package.describe({
   name: 'cultofcoders:apollo-accounts',
-  version: '3.2.3',
+  version: '3.3.0',
   // Brief, one-line summary of the package.
   summary: 'Meteor accounts in GraphQL',
   // URL to the Git repository containing the source code for this package.
